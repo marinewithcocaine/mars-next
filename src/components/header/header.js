c4dcd3c2cf4dc0021a473d540a4d7eac1268fe3c
Стас <marinewc@Noutbuk-Stas.local>
2025-09-09 08:39:14 +0200

    add link

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,7 +15,7 @@ export default function Header() {
                         </Link>
                     </li>
                     <li className={styles.nav_item}>
-                        <Link href='/cities' className={styles.nav_link}>
+                        <Link href='https://marspetnutritional.tilda.ws/better_cities' className={styles.nav_link}>
                             <svg className={`${styles.svg} ${styles.svg1}`} id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21.72 14.94">
                                 <path d="m.1,4.92c.06-.17.16-.32.29-.45L4.46.4c.53-.53,1.4-.53,1.93,0s.53,1.4,0,1.93l-1.77,1.75h13.02c2.25,0,4.07,1.82,4.07,4.07v5.43c0,.75-.61,1.36-1.36,1.36s-1.36-.61-1.36-1.36v-5.43c0-.75-.61-1.36-1.36-1.36H4.62l1.77,1.75c.53.53.54,1.39,0,1.92,0,0,0,0,0,0-.53.53-1.39.54-1.92,0,0,0,0,0,0,0L.39,6.4c-.12-.13-.22-.28-.29-.45-.14-.33-.14-.7,0-1.03Z" />
                             </svg>
